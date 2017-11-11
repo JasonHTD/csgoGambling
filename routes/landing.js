abeed53c2548a1a08c82b8581c3453fa6f90cd6a
JasonHTD <Jasonhtd11@hotmail.com>
2017-11-11 10:22:16 -0500

    Added to landing html and css

--- routes/landing.js
+++ routes/landing.js
@@ -4,12 +4,14 @@ var router = express.Router();
 var loadCSS = require("../middlewares/loadCSS");
 var loadJS = require("../middlewares/loadJS");
 
+
 router.use(loadCSS([
-  "css/landing.css"
+  "css/landing.css",
+  "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
 ]));
 
 router.use(loadJS([
-  
+
 ]));
 
 router.get("/", function(req, res, next){
